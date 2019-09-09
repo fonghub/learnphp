4934f0231aeba7384e6cf6ff5d999c4ddca762d4
zaofengchen <zaofengchen@163.com>
2019-09-09 16:53:55 +0800

    ..

--- Application/Task/Bll/Exception/TesException.class.php
+++ Application/Task/Bll/Exception/TesException.class.php
@@ -3,12 +3,8 @@
 namespace Task\Bll\Exception;
 
 use Think\Exception;
-use Throwable;
 
 class TesException extends Exception
 {
-    public function __construct($message = "", $code = 0, Throwable $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
+
 }
