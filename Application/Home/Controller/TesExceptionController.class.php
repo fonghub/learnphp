2d25b86b46896c1d326f2c74e79b1f64ac76dac9
zaofengchen <zaofengchen@163.com>
2019-09-11 10:50:58 +0800

    错误和异常

--- Application/Home/Controller/TesExceptionController.class.php
+++ Application/Home/Controller/TesExceptionController.class.php
@@ -25,4 +25,40 @@ class TesExceptionController
             echo $e->getMessage();
         }
     }
+
+
+    public function handlerException()
+    {
+        $this->userDefinedException();
+        throw new Exception("I am Exception<br>");
+
+    }
+
+    public function userDefinedException()
+    {
+        $myException = function ($exception){
+            echo $exception->getMessage();
+        };
+        set_exception_handler($myException);
+    }
+
+    public function handlerError()
+    {
+        $this->userDefinedError();
+        echo $test;
+    }
+
+    public function userDefinedError()
+    {
+        $myError = function ($errno,$errstr,$errfile,$errline){
+            $str = <<<EOF
+                "errno":$errno
+                "errstr":$errstr
+                "errfile":$errfile
+                "errline":$errline
+EOF;
+            echo $str;
+        };
+        set_error_handler($myError);
+    }
 }
